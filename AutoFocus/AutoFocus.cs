80fd580aa727d01608c566438b56dc8d98d1cd8b
BradDesktop <brad.martin@xdisplay.com>
2021-04-09 13:13:59 -0400

    cleanup and comment

--- AutoFocus/AutoFocus.cs
+++ AutoFocus/AutoFocus.cs
@@ -12,22 +12,31 @@ namespace AutoFocus
     {
         private string[] files;
         private List<double> scores = new List<double>();
+        private int[] focusTiles;
 
         public AutoFocus()
         {
             InitializeComponent();
             files = Directory.GetFiles(@"S:\RedFocus\Examples\Test");
 
-            foreach (string f in files)
-                scores.Add(ScoreImageFocus(new Bitmap(Image.FromFile(f))));
+            // Get tiles from first image in directory
+            focusTiles = GetTiles(new Bitmap(Image.FromFile(files[0])));
 
+            // Score each image
+            foreach (string f in files)
+                scores.Add(ScoreImageGrid(new Bitmap(Image.FromFile(f)), focusTiles));
+
+            // Scatterplot of scores
             var xs = Enumerable.Range(0, files.Length).Select(number => (double) number);
             Plot.plt.PlotScatter(xs.ToArray(), scores.ToArray());
+            // Note that you have to click the plot after form is shown for it to load
 
-            double MaxScore = scores.Max();
+            double MaxScore = scores.Max(); // Max is usually the best
             int BestIdx = scores.IndexOf(MaxScore);
+
             Bitmap bitmap = new Bitmap(Image.FromFile(files[BestIdx]));
-            HighlightGrid(ref bitmap);
+            HighlightTiles(ref bitmap, focusTiles); // Show what grid was used
+
             Panel.BackgroundImage = bitmap;
             Plot.plt.PlotPoint(BestIdx, MaxScore);
         }
