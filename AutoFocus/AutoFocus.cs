77b5d29a32e566480617a0abdebea7971202c088
BradDesktop <brad.martin@xdisplay.com>
2022-01-03 11:23:32 -0500

    comment and cleanup

--- AutoFocus/AutoFocus.cs
+++ AutoFocus/AutoFocus.cs
@@ -11,13 +11,14 @@ namespace AutoFocus
 {
     public partial class AutoFocus : Form
     {
+        private double _ImageScale = 1; // CONFIG
+
         private string[] _Files;
         private (int, int)[] _FocusTiles;
         private List<double> _Scores = new List<double>();
         private double _MaxScore;
         private int _BestIDX;
-        private string _BestFileName;
-        private double _ImageScale = 0.2; // EDITABLE
+        
         private Stopwatch _SW = new Stopwatch();
 
         public AutoFocus()
@@ -35,20 +36,19 @@ namespace AutoFocus
         {
             var xs = Enumerable.Range(0, _Files.Length).Select(number => (double)number);
             Plot.plt.PlotScatter(xs.ToArray(), _Scores.ToArray());
-            Plot.plt.PlotPoint(_BestIDX, _MaxScore);
+            Plot.plt.PlotPoint(_BestIDX, _MaxScore); // Makes it a new color
             Plot.plt.XLabel("Image #");
             Plot.plt.YLabel("Focus Score");
             Plot.plt.Title(string.Format("Using {0} Tile{1}", _FocusTiles.Length, _FocusTiles.Length > 1 ? "s" : ""));
             Plot.Visible = true;
-            Plot.ScrollWheelProcessor();
+            Plot.ScrollWheelProcessor(); // Makes the plot show wihtout user interation
         }
 
         private void ShowImage()
         {
             Bitmap bitmap = new Bitmap(Image.FromFile(_Files[_BestIDX]));
-            Bitmap resizedImage = new Bitmap(bitmap, new Size((int)Math.Round(bitmap.Width * _ImageScale), (int)Math.Round(bitmap.Height * _ImageScale)));
-            HighlightTiles(ref resizedImage, _FocusTiles); // Show what grid was used
-            Pbx.Image = resizedImage;
+            HighlightTiles(ref bitmap, _FocusTiles);
+            Pbx.Image = bitmap;
         }
 
         private void FocusWorker_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
@@ -61,7 +61,7 @@ namespace AutoFocus
             {
                 bitmap = new Bitmap(Image.FromFile(f));
                 resizedImage = new Bitmap(bitmap, new Size((int)Math.Round(bitmap.Width * _ImageScale), (int)Math.Round(bitmap.Height * _ImageScale)));
-                _Scores.Add(ScoreImageTiles(resizedImage, _FocusTiles));
+                _Scores.Add(ScoreImage(resizedImage, _FocusTiles));
             }
 
             e.Result = true; // Complete the background job
@@ -71,14 +71,13 @@ namespace AutoFocus
         {
             _SW.Stop();
 
-            _MaxScore = _Scores.Max(); // Max is usually the best
+            _MaxScore = _Scores.Max();
             _BestIDX = _Scores.IndexOf(_MaxScore);
-            _BestFileName = _Files[_BestIDX];
 
             MakePlot();
             ShowImage();
 
-            FileInfo fileInfo = new FileInfo(_BestFileName);
+            FileInfo fileInfo = new FileInfo(_Files[_BestIDX]);
             Text = $"{fileInfo.Name}   {Math.Round(_SW.Elapsed.TotalSeconds, 3)}s";
         }
     }
--- AutoFocus/Score.cs
+++ AutoFocus/Score.cs
@@ -4,16 +4,57 @@ using System.Drawing;
 using System.Drawing.Imaging;
 using System.Linq;
 using System.Runtime.InteropServices;
-using System.Threading.Tasks;
 
 namespace AutoFocus
 {
     static class Score
     {
-        private static int _GridSize = 15;
-        private static double _AmountDataDesired = 0.1;
+        private static int _GridSize = 15; // CONFIG
+        private static double _AmountDataDesired = 0.1; // CONFIG
 
-        public static double ScoreImageTiles(Bitmap bmp, (int, int)[] tiles)
+        #region Public Methods
+
+        /// <summary>
+        /// Get tiles with highest
+        /// entropies within image
+        /// </summary>
+        /// <param name="bmp"></param>
+        /// <returns>
+        /// (int, int) tuple
+        /// where item1 = Rect.X
+        /// and item2 = Rect.Y
+        /// </returns>
+        public static (int, int)[] GetTiles(Bitmap bmp)
+        {
+            Rectangle bmpRect = new Rectangle(Point.Empty, bmp.Size);
+            BitmapData bmpData = bmp.LockBits(bmpRect, ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb);
+            int size = bmpData.Stride * bmpData.Height;
+            byte[] data = new byte[size];
+            Marshal.Copy(bmpData.Scan0, data, 0, size);
+
+            Size scanSize = new Size(bmp.Width / _GridSize, bmp.Height / _GridSize);
+            List<(int, int, double)> tiles = new List<(int, int, double)>();
+
+            for (int i = 0; i < bmp.Width; i += scanSize.Width)
+                for (int j = 0; j < bmp.Height; j += scanSize.Height)
+                {
+                    Rectangle rect = new Rectangle(i, j, scanSize.Width, scanSize.Height);
+                    if (bmpRect.Contains(rect)) tiles.Add((i, j, GetCropEntropyARGB(data, rect, bmpData.Stride)));
+                }
+
+            tiles.Sort((x, y) => y.Item3.CompareTo(x.Item3)); // Sort list by greatest entropy to smallest entropy
+            (int, int)[] selectedTiles = tiles.Take((int)(tiles.Count() * _AmountDataDesired)).Select(x => (x.Item1, x.Item2)).ToArray();
+            return selectedTiles;
+        }
+
+        /// <summary>
+        /// Return average of scores
+        /// for desired tiles in image
+        /// </summary>
+        /// <param name="bmp"></param>
+        /// <param name="tiles"></param>
+        /// <returns></returns>
+        public static double ScoreImage(Bitmap bmp, (int, int)[] tiles)
         {
             Rectangle bmpRect = new Rectangle(Point.Empty, bmp.Size);
             BitmapData bmpData = bmp.LockBits(bmpRect, ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb);
@@ -30,96 +71,11 @@ namespace AutoFocus
                 if (bmpRect.Contains(rect)) scores.Add(ScoreTile(data, rect, bmpData.Stride));
             }
 
-            scores.RemoveAll(x => double.IsNaN(x));
             return scores.Average();
         }
 
-        private static double ScoreTile(byte[] data, Rectangle tile, int stride)
-        {
-            List<double> PDiff = new List<double>();
-
-            for (int i = tile.Left; i < tile.Right; i++)
-                for (int j = tile.Top; j < tile.Bottom; j++)
-                {
-                    int idx = i * 3 + j * stride;
-                    if (idx + 2 < data.Length)
-                    {
-                        double p1 = data[idx + 2];
-                        double localPDiff = 0;
-                        int counts = 0;
-                        for (int k = i - (int)(0.5 / _AmountDataDesired); k < i + (int)(0.5 / _AmountDataDesired); k++)
-                            for (int l = j - (int)(0.5 / _AmountDataDesired); l < j + (int)(0.5 / _AmountDataDesired); l++)
-                            {
-                                int neighborIdx = k * 3 + l * stride;
-                                if (neighborIdx + 2 > 0 && neighborIdx + 2 < data.Length)
-                                {
-                                    double p2 = data[neighborIdx + 2];
-                                    localPDiff += Math.Abs(p1 - p2) / ((p1 + p2) / 2);
-                                    counts++;
-                                }
-                            }
-                        PDiff.Add(localPDiff / counts);
-                    }
-                }
-
-            return PDiff.Average();
-        }
-
-        // Returns the tiles within a grid that have entropies in the highest 2 histogram bins
-        public static (int, int)[] GetTiles(Bitmap bmp)
-        {
-            Rectangle bmpRect = new Rectangle(Point.Empty, bmp.Size);
-            BitmapData bmpData = bmp.LockBits(bmpRect, ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb);
-            int size = bmpData.Stride * bmpData.Height;
-            byte[] data = new byte[size];
-            Marshal.Copy(bmpData.Scan0, data, 0, size);
-
-            Size scanSize = new Size(bmp.Width / _GridSize, bmp.Height / _GridSize);
-            List<(int, int, double)> tiles = new List<(int, int, double)>();
-
-            for (int i = 0; i < bmp.Width; i += scanSize.Width)
-                for (int j = 0; j < bmp.Height; j += scanSize.Height)
-                {
-                    Rectangle rect = new Rectangle(i, j, scanSize.Width, scanSize.Height);
-                    if (bmpRect.Contains(rect)) tiles.Add((i, j, GetRedCropEntropy(data, rect, bmpData.Stride)));
-                }
-
-            tiles.Sort((x, y) => y.Item3.CompareTo(x.Item3));
-            (int, int)[] selectedTiles = tiles.Take((int)(tiles.Count() * _AmountDataDesired)).Select(x => (x.Item1, x.Item2)).ToArray();
-            return selectedTiles;
-        }
-
         /// <summary>
-        /// Crop BMP Pixel Data,
-        /// take Red channel values,
-        /// and calculate entropy
-        /// for that tile
-        /// </summary>
-        /// <param name="data"></param>
-        /// <param name="tile"></param>
-        /// <param name="stride"></param>
-        /// <returns></returns>
-        private static double GetRedCropEntropy(byte[] data, Rectangle tile, int stride)
-        {
-            List<int> counts = new List<int>();
-            for (int i = tile.Left; i < tile.Right; i++)
-                for (int j = tile.Top; j < tile.Bottom; j++)
-                {
-                    int idx = i * 3 + j * stride;
-                    if (idx + 2 < data.Length)
-                        counts.Add(data[idx + 2] << 16 | data[idx + 1] << 8 | data[idx]);
-                }
-            double entropy = 0;
-            foreach (var g in counts.GroupBy(i => i))
-            {
-                double val = g.Count() / (double)(tile.Width * tile.Height);
-                entropy -= val * Math.Log2(val);
-            }
-            return entropy;
-        }
-
-        /// <summary>
-        /// Will colorize with the grid layout of the grid training image
+        /// Colorize tiles within image
         /// </summary>
         /// <param name="bmp"></param>
         /// <param name="tiles"></param>
@@ -133,5 +89,88 @@ namespace AutoFocus
                     g.FillRectangle(new SolidBrush(Color.FromArgb(100, Color.Green)), rect);
                 }
         }
+
+        #endregion
+
+        #region Private Methods
+
+        /// <summary>
+        /// Crop BMP Pixel Data
+        /// and calculate entropy
+        /// for that tile
+        /// </summary>
+        /// <param name="data">
+        /// BitmapData for current image
+        /// </param>
+        /// <param name="tile">
+        /// Rectangle to crop BitmapData
+        /// </param>
+        /// <param name="stride"></param>
+        /// <returns></returns>
+        private static double GetCropEntropyARGB(byte[] data, Rectangle tile, int stride)
+        {
+            List<int> counts = new List<int>(); // Each int is a ARGB value of a pixel
+            for (int i = tile.Left; i < tile.Right; i += (int)(1 / _AmountDataDesired))
+                for (int j = tile.Top; j < tile.Bottom; j += (int)(1 / _AmountDataDesired))
+                {
+                    int idx = i * 3 + j * stride; // Find starting index of pixel in data
+                    if (idx + 2 < data.Length) // Determine if it is wihtin the padding
+                        counts.Add(data[idx + 2] << 16 | data[idx + 1] << 8 | data[idx]);
+                    // Create ARGB int32 from RGB values which are ordered in the data array as BGR
+                }
+            double entropy = 0; // Calculate Shannon entropy
+            foreach (var g in counts.GroupBy(i => i)) // Turn list into an array of counts
+            {
+                double val = g.Count() / (double)(tile.Width * tile.Height);
+                entropy -= val * Math.Log2(val);
+            }
+            return entropy;
+        }
+
+        /// <summary>
+        /// Core of AutoFocus
+        /// </summary>
+        /// <param name="data">
+        /// BitmapData for current image
+        /// </param>
+        /// <param name="tile">
+        /// Rectangle to crop BitmapData
+        /// </param>
+        /// <param name="stride"></param>
+        /// <returns>
+        /// Calculated score for tile
+        /// </returns>
+        private static double ScoreTile(byte[] data, Rectangle tile, int stride)
+        {
+            List<double> PDiff = new List<double>(); // Turn the data array into a convolutional data array
+
+            for (int i = tile.Left; i < tile.Right; i += (int)(1 / _AmountDataDesired))
+                for (int j = tile.Top; j < tile.Bottom; j += (int)(1 / _AmountDataDesired))
+                {
+                    int idx = i * 3 + j * stride; // Find starting index of pixel in data
+                    if (idx + 2 < data.Length) // Determine if it is wihtin the padding
+                    {
+                        byte p1 = data[idx + 2]; // Get current pixel's Red channel value
+                        double localPDiff = 0; // Percent difference of p1 and neighboring p2's
+                        int counts = 0; // Number of p2's we found
+                        for (int k = i - (int)(0.5 / _AmountDataDesired); k < i + (int)(0.5 / _AmountDataDesired); k++)
+                            for (int l = j - (int)(0.5 / _AmountDataDesired); l < j + (int)(0.5 / _AmountDataDesired); l++)
+                            {
+                                int neighborIdx = k * 3 + l * stride;  // Find starting index of pixel in data
+                                if (neighborIdx + 2 > 0 && neighborIdx + 2 < data.Length)  // Determine if it is wihtin the array
+                                {
+                                    byte p2 = data[neighborIdx + 2]; // Get current pixel's Red channel value
+                                    localPDiff += Math.Abs(p1 - p2) / (double)((p1 + p2) / 2.0); // Add the calculated percent difference
+                                    counts++;
+                                }
+                            }
+                        PDiff.Add(localPDiff / counts); // Add the average to the list
+                    }
+                }
+
+            return PDiff.Average();
+        }
+
+        #endregion
     }
 }
